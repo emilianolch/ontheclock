5a2b6ca33d50d30933ec73564c510b59d1d833d7
Devskiller <owl@devskiller.com>
2023-08-17 12:34:31 +0000

    Update app/models/report.rb

--- app/models/report.rb
+++ app/models/report.rb
@@ -9,6 +9,19 @@ class Report
   validates :to, presence: true
   validates :employee_id, presence: true
   validate :date_range
+
+  def initialize(from:, to:, employee_id:)
+    self.from = from
+    self.to = to
+    self.employee_id = employee_id
+  end
   # TODO: represents the actual report, validate data and implement report methods
 
+  private
+
+  def date_range
+    return if from.is_a?(Date) && to.is_a?(Date) && to >= from
+
+    errors.add(:date_range, :invalid)
+  end
 end
