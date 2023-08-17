84bd077fd41bedfe42519405dcca50035f6e6288
Devskiller <owl@devskiller.com>
2023-08-17 12:44:46 +0000

    Update app/models/report_generator.rb

--- app/models/report_generator.rb
+++ app/models/report_generator.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
 class ReportGenerator
+  attr_reader :report
+
+  def initialize(**params)
+    @report = Report.new(params)
+  end
+
+  def generate
+    return if report.invalid?
+
+    events = Event.where("employee_id = ? AND timestamp >= ? AND timestamp <= ?",
+      report.employee_id,
+      report.from.beginning_of_day,
+      report.to.end_of_day)
   # TODO: - fetch range of events and generate Report
 end
