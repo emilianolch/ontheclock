55c9607a33b9bd66e619fd99eaeaf689c775e8b5
Devskiller <owl@devskiller.com>
2023-08-17 12:55:49 +0000

    Update app/models/report.rb

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,7 +3,7 @@
 class Report
   include ActiveModel::Validations
 
-  attr_accessor :from, :to, :employee_id, :worktime_hrs, :problematic_dates
+  attr_accessor :from, :to, :employee_id, :worktime, :problematic_dates
 
   validates :from, presence: true
   validates :to, presence: true
@@ -14,11 +14,24 @@ class Report
     @from = Date.new(from)
     @to = Date.new(to)
     @employee_id = employee_id
-    @worktime_hrs = 0.0
+    @worktime = 0.0
     @problematic_dates = []
   end
-  # TODO: represents the actual report, validate data and implement report methods
 
+  def worktime_hrs
+    (worktime / 3600).round(2)
+  end
+
+  def to_json
+    {
+      employee_id:,
+      from:,
+      to:,
+      worktime_hrs:,
+      problematic_dates:
+    }
+  end
+  
   private
 
   def date_range
