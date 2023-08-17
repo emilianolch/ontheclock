a3422f6755035cb3b277c2ec41e5a4beadd97fd9
Devskiller <owl@devskiller.com>
2023-08-17 12:31:25 +0000

    Update app/models/report.rb

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,16 +3,12 @@
 class Report
   include ActiveModel::Validations
 
-  attr_accessor :from, :to, :employee_id
+  attr_accessor :from, :to, :employee_id, :worktime_hrs, :problematic_dates
 
   validates :from, presence: true
   validates :to, presence: true
   validates :employee_id, presence: true
   validate :date_range
   # TODO: represents the actual report, validate data and implement report methods
-
-  private
-
-  
 
 end
