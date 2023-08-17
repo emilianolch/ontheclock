71820ebe36da6f53a4492c9859df14fa24b7a212
Devskiller <owl@devskiller.com>
2023-08-17 12:29:20 +0000

    Update app/controllers/reports_controller.rb

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,4 +2,19 @@
 
 class ReportsController < ApplicationController
   # TODO: implement report generation endpoint - it should delegate to ReportGenerator
+  def get
+    report = ReportGenerator.new(report_params).generate
+
+    if report
+      render json: report
+    else
+      head(:bad_request)
+    end
+  end
+
+  private
+
+  def report_params
+    params.permit(:from, :to, :employee_id)
+  end
 end
